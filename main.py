c0d973dcfa92d7381814d7095d3e21bf7d7a2446
ahernot <70817470+ahernot@users.noreply.github.com>
2022-03-26 18:59:37 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""
+Image bannermarking utility.
+@author: Anatole Hernot
+@version: 0.0.1
+"""
+
+import os
+
+if __name__ == '__main__':
+
+    # Change path to the parent directory
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
+
+
+    pass
---
+++ preferences.py
@@ -0,0 +1,10 @@
+INPUT_PATH = 'src/'
+EXTENSIONS_IMAGES = ('jpg', 'jpeg', 'tiff', 'png')
+
+
+# save as TIFF 600ppi
+
+
+# end landscape banners with '-landscape'
+# end portrait banners with '-portrait'
+# banners are placed at the bottom of images
